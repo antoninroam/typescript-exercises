8a922749d872e43a76932a6cc1d7e26683e34ead
Anton Lebedev <anton.lebedev@weareroam.com>
2022-04-26 13:21:45 +1200

    feat: add answers

---
+++ exercises/5_answer.tsx
@@ -0,0 +1,34 @@
+interface Success {
+  type: `${string}Success`;
+  body: string;
+}
+
+interface Error {
+  type: `${string}Error`;
+  message: string;
+}
+
+interface AccountPageProps {
+  response: Success | Error;
+}
+
+
+export const AccountPage: React.FC<AccountPageProps> = ({ response }) => {
+
+  const handleResponse = (response: Success | Error) => {
+    if (response.type === "HttpError") {
+      return "Please check your internet connection.";
+    } else if (response.type === "BackendError") {
+      return "Something went wrong on the server.";
+    } else if (response.type === "Success") {
+      return response.body;
+    }
+  }
+
+  return (
+    <div>
+      <h1>Account</h1>
+      <p>{handleResponse(response)}</p>
+    </div>
+  );
+}
